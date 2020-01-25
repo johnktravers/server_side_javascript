9a98d9196a8c217895bc5a592537063b6cc556ce
John Travers <johnk.travers@gmail.com>
2020-01-24 19:56:48 -0700

    Add post API routes to create papers and footnotes

--- app.js
+++ app.js
@@ -27,6 +27,38 @@ app.get('/api/v1/footnotes', (request, response) => {
     .catch(error => response.status(500).json({ error }));
 });
 
+app.post('/api/v1/papers', (request, response) => {
+  const paper = request.body;
+
+  for (let requiredParameter of ['title', 'author']) {
+    if (!paper[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: { title: <String>, author: <String> }. You're missing a "${requiredParameter}" property.` });
+    }
+  }
+
+  database('papers').insert(paper, 'id')
+    .then(paper => response.status(201).json({ id: paper[0] }))
+    .catch(error => response.status(500).json({ error }));
+});
+
+app.post('/api/v1/footnotes', (request, response) => {
+  const footnote = request.body;
+
+  for (let requiredParameter of ['note', 'paper_id']) {
+    if (!footnote[requiredParameter]) {
+      return response
+        .status(412)
+        .send({ error: `Expected format: { note: <String>, paper_id: <Integer> }. You're missing a "${requiredParameter}" property.` });
+    }
+  }
+
+  database('footnotes').insert(footnote, 'id')
+    .then(footnote => response.status(201).json({ id: footnote[0] }))
+    .catch(error => response.status(500).json({ error }));
+});
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
